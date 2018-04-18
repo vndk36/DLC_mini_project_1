cd103ff0efa5cd82d449342da902d940261a909b
vndk36 <victor.faraut@gmail.com>
2018-04-18 10:36:13 +0200

    TP5 parts added

--- MiniProject1_DLC.py
+++ MiniProject1_DLC.py
@@ -82,6 +82,58 @@ def load(root, train = True, download = True, one_khz = False):
         target = target.view(-1).long()
 
     return input, target
+######################################################################
+
+mini_batch_size = 100
+
+def train_model(model, train_input, train_target):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr = 1e-1)
+    nb_epochs = 250
+
+    for e in range(0, nb_epochs):
+        for b in range(0, train_input.size(0), mini_batch_size):
+            output = model(train_input.narrow(0, b, mini_batch_size))
+            loss = criterion(output, train_target.narrow(0, b, mini_batch_size))
+            model.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+#################################################################
+
+def compute_nb_errors(model, data_input, data_target):
+
+    nb_data_errors = 0
+
+    for b in range(0, data_input.size(0), mini_batch_size):
+        output = model(data_input.narrow(0, b, mini_batch_size))
+        _, predicted_classes = torch.max(output.data, 1)
+        for k in range(0, mini_batch_size):
+            if data_target.data[b + k] != predicted_classes[k]:
+                nb_data_errors = nb_data_errors + 1
+
+    return nb_data_errors
+            
+            
+#################################################################
+
+model = nn.Sequential(
+        nn.Linear(2, 4),
+        nn.ReLU(),
+        nn.Linear(4, 8),
+        nn.ReLU(),
+        nn.Linear(8, 16),
+        nn.ReLU(),
+        nn.Linear(16, 32),
+        nn.ReLU(),
+        nn.Linear(32, 64),
+        nn.ReLU(),
+        nn.Linear(64, 128),
+        nn.ReLU(),
+        nn.Linear(128, 2)
+    )
+
+
 
 
 # In[22]:
@@ -99,6 +151,8 @@ import matplotlib.pyplot as plt
 
 plt.plot(range(0,50),train_input[4,4,:].data.numpy())
 plt.show()
+######################################################################
+
 
 
 # In[ ]:
